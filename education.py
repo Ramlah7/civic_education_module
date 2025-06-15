b87fcc4c7823e4c23b2399f4ce014c0750f8688c
Ramlah <175896348+Ramlah7@users.noreply.github.com>
2025-06-15 22:59:27 +0500

    added topcs.json

--- education.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui.civic_education_gui import CivicEducationApp
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = CivicEducationApp()
-    window.show()
-    sys.exit(app.exec_())
